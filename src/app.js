9ddc5af307e8810e87737f5a8a224b118ff1c079
d0nn12 <pilarstrasburg@gmail.com>
2024-10-05 12:21:29 -0400

    injected image to sub emoji

--- src/app.js
+++ src/app.js
@@ -7,10 +7,7 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-
-  console.log(response.data);
-  console.log(response.data.temperature.humidity);
-  console.log(response.data.wind.speed);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -18,6 +15,10 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}mph`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 }
 
 function formatDate(date) {
